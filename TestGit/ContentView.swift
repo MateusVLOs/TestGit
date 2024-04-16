c7f5f76826e5be3861f436d4ac1c1d523e7fd44b
MateusVLOs <mateus-assis@outlook.com>
2024-04-16 11:10:37 -0300

    NavigationStack

--- TestGit/ContentView.swift
+++ TestGit/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                Section {
+                    ScrollView(.horizontal) {
+                        LazyHGrid(rows: [GridItem(.fixed(20))]) {
+                            ForEach(0..<5) { _ in
+                                Rectangle()
+                                    .fill(.blue)
+                                    .frame(width: 100, height: 150)
+                                    .clipShape(.rect(cornerRadius: 5))
+                            }
+                        }
+                        .padding()
+                    }
+                }
+            }
+            .navigationTitle("APP Name")
+            .toolbar {
+                Button("jogar deck aleatorio", systemImage: "shuffle") {
+                    
+                }
+                Button("Criar Deck", systemImage: "plus") {
+                    
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
